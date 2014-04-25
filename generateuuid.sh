30b488ea4ecf5dc0d4c6a48dfbf56e725388add6
Tony Edgin <tedgin@iplantcollaborative.org>
2014-04-25 12:46:43 -0700

    [CORE-5435] updated comment to mention uuidd

--- generateuuid.sh
+++ generateuuid.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -
 #
-# This script generates a UUID. For Linux systems, this will be a time-based UUID.
+# This script generates a UUID. For Linux systems, this will be a time-based UUID. To guarantee 
+# uniqueness among concurrent executions of this script, the uuidd daemon should be running.
 
 if [ "$(uname)" == Linux ]
 then
